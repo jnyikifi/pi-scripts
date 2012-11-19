d41abc0047cfc8f7a238542a372ab3fea257e936
Kristoffer Nyberg <jny@iki.fi>
2012-11-19 22:21:32 +0200

    First version of PI tvheadend backup

---
+++ backup/backup-to-hd.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+timestamp=`date '+%Y%m%d-%H%M%S'`
+filename="/home/pi/tv/backup/pi-${timestamp}.tar.gz"
+
+echo "Start $timestamp"
+
+dpkg-query --show > /home/pi/pkg.list
+
+cd /
+tar czvf $filename \
+    boot/config.txt \
+    home/pi/.hts/ \
+    etc/fstab \
+    home/pi/pkg.list
+
+echo -n "Done "
+date '+%Y%m%d-%H%M%S'
